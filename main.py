5bf7b3c4cd6c3373700c165f64bef6f68e2a4e6c
Anderson Guilherme <89460760+AndersonG2021@users.noreply.github.com>
2022-07-19 21:50:34 -0300

    Novo main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import mysql.connector
 from cliente import Cliente
 from prestador import Prestador
 
-print("Cavalo")
+
 conexao = mysql.connector.connect(
     host='localhost',
     user='root',
@@ -12,29 +12,58 @@ conexao = mysql.connector.connect(
 
 cursor = conexao.cursor()
 
-prestador = Prestador()
+prestador = Prestador(cursor, conexao)
 
-cliente = Cliente()
+cliente = Cliente(cursor, conexao)
 
 print("Cavalo")
 
 escolha = -1
-while escolha != 0:
+while escolha != '0':
     print("""BEM VINDO AO FRETEX SOMOS PAU PRA TODA OBRA!!
     Para prosseguir selecione alguma das opções a seguir:\n
         0 - Para sair do aplicativo!
         1 - Para entrar como um prestador!
         2 - Para entrar como um cliente! """)
 
-    escolha = input("Digite uma opção")
+    escolha = input("Digite uma opção:  ")
 
     if escolha == '1':
-
-        prestador.verificar_cadastro()
+        tipo='prestador'
+        cpf = prestador.verificar_cadastro(tipo)
+        decisao = -1
+        while decisao != '4':
+            print("""O que deseja fazer agora?
+                           1 - Buscar Frete
+                           2 - Para Iniciar frete
+                           3 - Para Editar informações de Perfil
+                           4 - Para Deslogar""")
+            decisao = input("Digite sua opção: ")
+            if decisao == '1' :
+                prestador.bucar_cliente()
+        else:
+            print("Usuário Deslogado!!\n")
+            print("Voltando para o Menu inicial\n\n")
 
     elif escolha == '2':
+        tipo = 'cliente'
+        cpf = cliente.verificar_cadastro(tipo)
 
-        cliente.verificar_cadastro()
+        decisao = '0'
+        while decisao != '5':
+            print("""O que deseja fazer agora?
+                    1 - Para solicitar frete
+                    2 - Para acompanhar frete
+                    3 - Para Alterar frete
+                    4 - Para Editar informações de Perfil
+                    5 - Para Deslogar""")
+            decisao = input("Digite sua opção: ")
+            if decisao == '1':
+                cliente.solicitar_frete(cpf)
+            elif decisao == '4':
+                cliente.alterar_cadastro(cpf)
+        else:
+            print("Usuário Deslogado!\n")
 
     elif escolha == '0':
         print("Volte Sempre!!!")
@@ -42,10 +71,8 @@ while escolha != 0:
 
 
 
-
-
-
-
+cursor.close()
+conexao.close()
 
 
 
