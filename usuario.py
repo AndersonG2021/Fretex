87a1896ebbbe3e317d80fb18185b7b60602c93d0
Anderson Guilherme <89460760+AndersonG2021@users.noreply.github.com>
2022-07-19 21:54:02 -0300

    Novo usuario.py

--- usuario.py
+++ usuario.py
@@ -1,14 +1,5 @@
 import mysql.connector
 
-conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='SHIPPuuden300',
-    database='fretex',
-)
-cursor = conexao.cursor()
-
-
 class Usuario():
     nome = ''
     cpf = ''
@@ -19,43 +10,58 @@ class Usuario():
     cidade = ''
     estado = ''
     pdr = ''
-    tipo = 'cliente'
+    tipo = ''
+    senha = ''
 
-    def logar(self):
-        print("Você está logado")
+    def __init__(self, cursor, conexao):
+        self.cursor = cursor
+        self.conexao = conexao
 
-    def verificar_cadastro(self):
-        self.decisao = input("Você já possui cadastro? y/n")
+    def logar(self,tipo):
+        self.tipo = tipo
+        self.cpf = input("Qual o seu CPF? \n")
+        self.senha = input("Digite sua Senha: ")
+        try:
+            comando = f'SELECT CPF FROM usuário WHERE CPF = "{self.cpf}"'
+            self.cursor.execute(comando)
+            resultado1 = self.cursor.fetchall()
+
+            comando = f'SELECT Senha FROM usuário WHERE Senha = "{self.senha}"'
+            self.cursor.execute(comando)
+            resultado2 = self.cursor.fetchall()
+
+            if (len(resultado1) > 0 and len(resultado2) > 0):
+                comando = f'SELECT Nome FROM usuário WHERE CPF = "{self.cpf}"'
+                self.cursor.execute(comando)
+                resultado = self.cursor.fetchall()
+
+                for i in resultado:
+                    self.nome = i[0]
+
+                print(f"Olá {self.nome} o que deseja fazer a seguir?\n")
+            else:
+                print("Desculpe você ainda tem um registro no nosso sistema :\n")
+                print("Mas não se preocupe vamos realizar seu cadastro agora!!")
+                self.cadastrar(self.tipo)
+
+        except Exception as err:
+            print("couldn't connect")
+            print("General error :: ", err)
+
+    def verificar_cadastro(self, tipo):
+        self.tipo = tipo
+        self.decisao = input("Você já possui cadastro? y/n ")
         if self.decisao == 'n':
-            self.cadastrar()
+            self.cadastrar(self.tipo)
         else:
-            self.cpf = input("Qual o seu CPF? \n")
-            try:
-                comando = f'SELECT CPF FROM usuário WHERE CPF = "{self.cpf}"'
-                cursor.execute(comando)
-                resultado = cursor.fetchall()
+            self.logar(self.tipo)
+        return self.cpf
 
-                if (len(resultado) > 0):
-                    comando = f'SELECT Nome FROM usuário WHERE CPF = "{self.cpf}"'
-                    cursor.execute(comando)
-                    resultado = cursor.fetchall()
-
-                    for i in resultado:
-                        self.nome = i[0]
-
-                    print(f"Olá {self.nome} o que deseja fazer a seguir?\n")
-                else:
-                    print("Desculpe você ainda tem um registro no nosso sistema :\n")
-                    print("Mas não se preocupe vamos realizar seu cadastro agora!!")
-                    self.cadastrar()
-
-            except Exception as err:
-                print("couldn't connect")
-                print("General error :: ", err)
-
-    def cadastrar(self):
+    def cadastrar(self, tipo):
+        self.tipo = tipo
         print("Para criar seu cadastro por favor preencha as informações a seguir:")
         self.nome = input("Nome: ")
+        self.senha = input("Digite sua senha: ")
         self.cpf = input("CPF: ")
         self.email = input("E-mail: ")
         self.logradouro = input("Logradouro: ")
@@ -65,20 +71,61 @@ class Usuario():
         self.estado = input("Estado: ")
         self.pdr = input("Ponto de Referência: ")
 
-        comando = f'INSERT INTO Usuário (CPF, Nome, email,Tipo) VALUES ("{self.cpf}","{self.nome}","{self.email}","{self.tipo}")'
-        cursor.execute(comando)
-        conexao.commit()
+        comando = f'INSERT INTO usuário (CPF, Senha, Nome, email,Tipo) VALUES ("{self.cpf}","{self.senha}","{self.nome}","{self.email}","{self.tipo}")'
+        self.cursor.execute(comando)
+        self.conexao.commit()
 
         comando = f'INSERT INTO endereco_cadastro (CPF,Logradouro, CEP, Bairro, Cidade, Estado, Ponto_de_referencia) VALUES ("{self.cpf}","{self.logradouro}",{self.cep},"{self.bairro}","{self.cidade}","{self.estado}","{self.pdr}")'
-        cursor.execute(comando)
-        conexao.commit()
+        self.cursor.execute(comando)
+        self.conexao.commit()
 
         print("Aguarde enquanto validamos seu e-mail")
         print("E-mail validado!! \n\n Seu cadastro foi efetuado com sucesso !!!")
+        return self.cpf
+
+    def alterar_cadastro(self,cpf):
+        self.cpf = cpf
+        decisao = -1
+        while decisao != '0':
+            decisao = input("""Qual dado você deseja alterar?
+                    1 - Nome
+                    2 - Endereço
+                    0 - Voltar para o perfil""")
+
+            if decisao == '1':
+                print("Digite seu dados corretamente: ")
+                self.nome = input("Digite Seu nome: ")
+                self.email = input("Digite seu e-mail: ")
+                self.senha = input("Digite sua senha: ")
+                comando = f'UPDATE usuário SET Nome = "{self.nome}", email = "{self.email}", Senha = "{self.senha}" WHERE CPF = "{self.cpf}"'
+                self.cursor.execute(comando)
+                self.conexao.commit()
+
+            if decisao == '2':
+                print("Nos infome seu endereço corretamente: \n")
+                self.logradouro = input("\nLogradouro: ")
+                self.cep = int(input("\nCep: "))
+                self.bairro = input("\nBairro: ")
+                self.cidade = input("\nCidade: ")
+                self.estado = input("\nEstado: ")
+                self.pdr = input("\nPonto de Referência: ")
+                comando = f'UPDATE endereco_cadastro SET Logradouro = "{self.logradouro}", CEP = "{self.cep}", Bairro = "{self.bairro}", Cidade = "{self.cidade}", Estado = "{self.estado}", Ponto_de_referencia = "{self.pdr}" WHERE CPF = "{self.cpf}"'
+                self.cursor.execute(comando)
+                self.conexao.commit()
+            elif decisao == '0':
+                comando = f'SELECT usuário.Nome, usuário.Email, usuário.Senha, endereco_cadastro.Logradouro, endereco_cadastro.CEP, endereco_cadastro.Bairro, endereco_cadastro.Cidade, endereco_cadastro.Estado, endereco_cadastro.Ponto_de_referencia FROM usuário INNER JOIN endereco_cadastro ON usuário.CPF = endereco_cadastro.CPF AND usuário.Tipo = "cliente"'
+                self.resultado = 0
+                self.cursor.execute(comando)
+                resultado = self.cursor.fetchall()
+                for i in resultado:
+                    print(i)
+
+                print("Seus dados foram alterados")
+                print("Voltando para página de perfil !!\n\n")
+
+
 
     def sair(self):
         print("Volte Sempre!!")
 
 
-cursor.close()
-conexao.close()
